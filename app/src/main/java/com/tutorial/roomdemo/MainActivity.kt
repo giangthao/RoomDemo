c213d506161e0ee28cf8457517c6594e9ff5da27
giangthao <thaogiang.90@gmail.com>
2022-08-09 17:23:16 +0700

    Add Cycle view

--- app/src/main/java/com/tutorial/roomdemo/MainActivity.kt
+++ app/src/main/java/com/tutorial/roomdemo/MainActivity.kt
@@ -2,8 +2,11 @@ package com.tutorial.roomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tutorial.roomdemo.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
@@ -18,11 +21,18 @@ class MainActivity : AppCompatActivity() {
             // TODO call addrecord with employeeDao
             addRecord(employeeDao)
         }
+        lifecycleScope.launch{
+            employeeDao.fetchAllEmployees().collect{
+                Log.d("exactemployee","$it")
+                val list = ArrayList(it)
+                setupListOfDataIntoRecyclerView(list,employeeDao)
+            }
+        }
     }
     fun addRecord(employeeDao: EmployeeDao){
         val name = binding?.etName?.text.toString()
         val email = binding?.etEmailId?.text.toString()
-        if(email.isNotEmpty() && email.isNotEmpty()){
+        if(email.isNotEmpty() && name.isNotEmpty()){
             lifecycleScope.launch{
                 employeeDao.insert(EmployeeEntity(name = name, email = email))
                 Toast.makeText(applicationContext,"Record saved",Toast.LENGTH_LONG)
@@ -35,6 +45,28 @@ class MainActivity : AppCompatActivity() {
                 "Name or Email cannot be blank",
                 Toast.LENGTH_LONG
             ).show()
+        }
+    }
+    /**
+     * Function is used show the list of inserted data.
+     */
+    private fun setupListOfDataIntoRecyclerView(employeesList:ArrayList<EmployeeEntity>,
+                                                employeeDao: EmployeeDao) {
+
+        if (employeesList.isNotEmpty()) {
+            val itemAdapter = ItemAdapter(employeesList)
+
+
+            // Set the LayoutManager that this RecyclerView will use.
+            binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
+            // adapter instance is set to the recyclerview to inflate the items.
+            binding?.rvItemsList?.adapter = itemAdapter
+            binding?.rvItemsList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+        } else {
+
+            binding?.rvItemsList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
     }
 }
